60b9122e23e5ddaf579d3b8d193eabf0e1189c4d
Александр Гаврилов <aleksandrgavrilov7229@gmail.com>
2024-12-21 21:34:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json;
+using ClassLibrary08;
+
+class Program
+{
+    public static void Main()
+    {
+        Lion lion = new Lion("India", "Leo");
+
+        //Сериализация
+        string jsonstring = JsonSerializer.Serialize(lion);
+        Console.WriteLine(jsonstring);
+    }
+}
